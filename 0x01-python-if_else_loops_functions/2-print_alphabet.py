508b83cdd5b25dcbf5f909cd0b57ae87381bb574
Awotayo <mabolaje.jamiu@gmail.com>
2022-11-30 11:59:31 +0000

    python prj

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+# 2-print_alphabet.py
+# Brennan D Baraban <375@holbertonschool.com>
 for letters in range(97, 123):
-    print(f"{chr(letters)}", end="")
+    print("{}".format(chr(letters)), end="")
