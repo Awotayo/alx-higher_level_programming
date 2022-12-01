a956afc4d00375c9e0a1951d66cc43ca736a10eb
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-01 02:21:31 +0000

    python prj

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 10 == 3:
+            print("Fizz ", end="")
+        elif number % 10 == 5:
+            print("Buzz ", end="")
+        elif number % 10 == 15:
+            print("FizzBuzz ", end="")
+        else:
+            print("number ", end="")
