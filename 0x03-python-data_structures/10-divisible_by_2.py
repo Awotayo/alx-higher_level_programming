fb93b5773362712348f6507b91f93a709975e8bf
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-06 04:01:39 +0000

    python prj

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     r_list = []
     for i in range(len(my_list)):
-        if my_list[1] % 2 == 0:
+        if my_list[i] % 2 == 0:
             r_list.append(True)
         else:
             r_list.append(False)
