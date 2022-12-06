921435ece493cee439f4e4d00564e0234c96b656
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-06 03:52:21 +0000

    python prj

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    r_list = []
+    for i in range(len(my_list)):
+        if my_list[1] % 2 == 0:
+            view = True
+        else:
+            view = False
+    return (r_list.append(view))
