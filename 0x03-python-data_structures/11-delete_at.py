23c69ad40fe1079cf7f33c82490765193b84eb11
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-06 04:24:11 +0000

    python prj

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    final = []
-    for i in range(len(my_list)):
-        if i == idx:
-            if i <= 0 and i < len(my_list):
-                return (my_list)
-            my_list.remove(i)
-            final.append(i)
-    return(final)
+    if idx <= 0 and idx < len(my_list):
+        my_list.remove(idx)
+    return(my_list)
