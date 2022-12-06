3923ea52db875778c0c38d35d5541513be9f50df
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-06 04:15:33 +0000

    python prj

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    final = []
+    for i in range(len(my_list)):
+        if i == idx:
+            if i <= 0 and i < len(my_list):
+                return (my_list)
+            my_list.remove(i)
+        final.append(i)
+    return(fianl)
