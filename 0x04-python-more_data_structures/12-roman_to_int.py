8d9d112f74b2a7ed4b523d799564d704434c7410
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-08 11:38:08 +0000

    python prj

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    def roman_to_int(roman_string):
-        roman = {
-                "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-                "D": 500, "DM": 900,
-                "M": 1000, "IV": 4, "IX": 9, "XL": 40,
-                "XC": 90, "CD": 400
-                }
-        if not isinstance(roman_string, str):
-            return 0
-        num = 0
-        digits = 0
-        len = len(roman_string)
-        while digits < len:
-            if digits + 1 < len and roman_string[digits:digits + 2] in roman:
-                : num += roman[roman_string[digits:digits + 2]]
-                digits += 2
-            else:
-                num += roman[roman_string[digits]]
-                digits += 1
-        return num
+    roman = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
+            "D": 500, "DM": 900,
+            "M": 1000, "IV": 4, "IX": 9, "XL": 40,
+            "XC": 90, "CD": 400
+            }
+    num = 0
+
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] <
+                roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1
+        else:
+            num += roman_dict[roman_string[i]]
+    return (num)
