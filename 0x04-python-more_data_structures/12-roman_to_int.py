104f04d0db7bdd6ad93df48cf04d2ecf725579f1
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-08 12:09:13 +0000

    python prj

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,15 @@
 def roman_to_int(roman_string):
     roman = {
             "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-            "D": 500, "DM": 900,
-            "M": 1000, "IV": 4, "IX": 9, "XL": 40,
-            "XC": 90, "CD": 400
+            "D": 500, "M": 1000
             }
     num = 0
+    cnt = len(roman_string)
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] <
-                roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
+    for (id, c) in enumerate(roman_string):
+        if id < cnt - 1 and roman[c] < roman[roman_string[id + 1]):
+            num -= roman[c]
+
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            num += roman[c]
+    return num
