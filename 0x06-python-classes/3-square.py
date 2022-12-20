426143c43e8281f6ad75a8b1bbb586c793adafb0
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-20 22:05:01 +0000

    python prj

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,17 +3,19 @@
 
 
 class Square:
-    """ Square Class
-    Attributes:
-    size (int): size of square
-    """
-    __size = 0
+    """ Square Class"""
+
     def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
         def area(self):
-            A = self.__size * self.__size
-            return (A)
+            """Return the current area of the square."""
+            return (self.__size * self.__size)
